7a0da7641ede3e82140414ed32510ade79ea8e6b
Dan Stronge <dnlstronge@gmail.com>
2023-05-18 08:11:23 +0100

    [x] - set up state slice and reducers

--- src/Store/UI-slice.js
+++ src/Store/UI-slice.js
@@ -1,2 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+
+
+createSlice({
+    name: "ui",
+    initialState: {cartIsVisable: false},
+    reducers: {
+        showCart(state) {
+            state.cartIsVisable = true
+        },
+        hideCart(state) {
+            state.cartIsVisable = false
+        }
+    }
+})

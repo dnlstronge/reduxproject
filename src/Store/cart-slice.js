10e7fff6294b11f64b8e9479d0c016ae87292487
Dan Stronge <dnlstronge@gmail.com>
2023-05-18 10:01:04 +0100

    [x] - add logic

--- src/Store/cart-slice.js
+++ src/Store/cart-slice.js
@@ -12,7 +12,11 @@ const cartSlice = createSlice({
             const newItem = action.payload,
             existingItem = state.items.find(item => item.id === newItem.id)
             if(!existingItem) {
-                state.items.push(newItem)
+                state.items.push({
+                    itemId: newItem.id, 
+                    price: newItem.price, 
+                    quantity: 1, 
+                    totalPrice: newItem.price})
             }
         },
         removeItemFromCart() {},

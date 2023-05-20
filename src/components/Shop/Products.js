a6309ffdd03da5555484529b12e47b29ab723e27
Dan Stronge <dnlstronge@gmail.com>
2023-05-20 09:48:18 +0100

    [x] - map dummy items an Output as products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -21,11 +21,16 @@ const Products = (props) => {
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+      {DUMMY_PRODUCTS.map((item) => {
+        return (
+          <ProductItem
+          title={item.title}
+          price={item.price}
+          description={item.description}
         />
+        )
+      })}
+        
       </ul>
     </section>
   );

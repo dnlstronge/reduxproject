b75c47803edf5291aa7ff09d59759c247c42dcd3
Dan Stronge <dnlstronge@gmail.com>
2023-05-20 10:22:45 +0100

    [x] - add logic

--- src/Store/cart-slice.js
+++ src/Store/cart-slice.js
@@ -11,6 +11,7 @@ const cartSlice = createSlice({
         addItemToCart(state, action) {
             const newItem = action.payload,
             existingItem = state.items.find(item => item.id === newItem.id)
+            existingItem.quantity++
             if(!existingItem) {
                 state.items.push({
                     itemId: newItem.id, 
@@ -20,17 +21,18 @@ const cartSlice = createSlice({
                     name: newItem.title
                 })
             } else {
-                existingItem.quantity++
+                //existingItem.quantity++
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price
             }
         },
         removeItemFromCart(state, action) {
             const id = action.payload
             const existingItem = state.items.find(item => item.id === id)
+            existingItem.quantity--
             if(existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.id !== id)
             } else {
-                existingItem.quantity--
+                //existingItem.quantity--
                 existingItem.totalPrice = existingItem.totalPrice - existingItem.price
             }
         },

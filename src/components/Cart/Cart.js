4e567baa2277a640693d0f824c0259502c9d52e8
Dan Stronge <dnlstronge@gmail.com>
2023-05-21 08:58:37 +0100

    [x] - map items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,22 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
+
+
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart.items)
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
+      {cartItems.map((cartItem) => {return (
+        <CartItem
+          item={{ title: cartItem.title, quantity: cartItem.quantity, total: 18, price: 6 }}
+        />
+      )} )}
         <CartItem
           item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
         />

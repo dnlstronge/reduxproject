ae4488ea7e78914d81b24423df80f03df19a5156
Dan Stronge <dnlstronge@gmail.com>
2023-05-22 08:35:52 +0100

    [x] - add selector

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,12 +1,12 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { cartActions } from '../../Store/cart-slice';
 
 
 const ProductItem = (props) => {
   const { title, price, description, id } = props;
-
+  const cart = useSelector((state) => state.cart)
   const dispatch = useDispatch()
 
   const handleAddToCart = () => {

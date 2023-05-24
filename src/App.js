d6a1219f0335d24eefdf4d6596fafa6708c56741
Dan Stronge <dnlstronge@gmail.com>
2023-05-24 08:19:23 +0100

    [x] - add useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Products from './components/Shop/Products'
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 function App() {
 
   const showCart = useSelector((state) => state.ui.cartIsVisible)
-  useSelector((state) => state.cart)
+  const cart = useSelector((state) => state.cart)
+  
+  useEffect(() => {
+
+    // send post
+  }, [cart])
 
   return (
     <Layout>

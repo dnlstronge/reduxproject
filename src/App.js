8d38eb58d8832db4be849b029867e409c60cdd6f
Dan Stronge <dnlstronge@gmail.com>
2023-05-26 08:03:49 +0100

    [x] - move cart actions to separate file

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       isInitial = false;
       return;
     }
-      sendCartData()
+      dispatch(sendCartData(cart))
   }, [cart, dispatch]);
 
   return (
--- src/Store/cart-slice.js
+++ src/Store/cart-slice.js
@@ -43,54 +43,7 @@ const cartSlice = createSlice({
 
 })
 
-export const sendCartData = (cart) => {
-    return async (dispatch) => {
-        dispatch(
-            uiSliceActions.showNotification({
-              status: 'pending',
-              title: 'Sending...',
-              message: 'Sending cart data!',
-            })
-          );
-          const sendRequest = async() => {
-            const response = await fetch(
-                'https://connectdb-1efa3-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-                {
-                  method: 'PUT',
-                  body: JSON.stringify(cart),
-                }
-              );
-        
-              if (!response.ok) {
-                throw new Error('Sending cart data failed.');
-              }
-          };
 
-
-          try {
-            await sendRequest()
-            dispatch(
-                uiSliceActions.showNotification({
-                  status: 'success',
-                  title: 'Success!',
-                  message: 'Sent cart data successfully!',
-                })
-              );
-          } catch (error) {
-            dispatch(
-                uiSliceActions.showNotification({
-                  status: 'error',
-                  title: 'Error!',
-                  message: 'Sending cart data failed!',
-                })
-              );
-          }
-         
-
-         
-    }
-    
-}
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
